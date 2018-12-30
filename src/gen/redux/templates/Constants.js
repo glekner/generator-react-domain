2e6aea026656a850947a28a3e13df2ffac16c5df
Gilad Lekner <gilad215@gmail.com>
2018-12-30 21:26:29 +0200

    fixing LF :fire:

--- src/gen/redux/templates/Constants.js
+++
@@ -1,1 +0,0 @@
-export const <%= name_upper %>_CHANGE_BOOL = '<%= name_upper %>_CHANGE_BOOL';
--- src/gen/redux/templates/Selectors.js
+++
@@ -1,2 +0,0 @@
-export const select<%= name %> = state => state.<%= name_lower %>;
-export const selectBool = state => select<%= name %>(state).bool;
